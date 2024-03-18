e6c1552b1195f546e2723006e7114c7ae4198b00
Abhishek KumarJha <jha.abhishek001@gmail.com>
2024-03-18 18:02:41 +0530

    react dango new1

--- src/App.js
+++ src/App.js
@@ -1,73 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios'
- 
-const Blog = [
-  {
-      "id": 1,
-      "title": "Python",
-      "content": "Python is the most used language in top company",
-      "author": "Abhishek",
-      "date_created": "2024-03-08T13:20:29.298153Z"
-  },
-  {
-      "id": 2,
-      "title": "Java",
-      "content": "Java is the most used language in top company",
-      "author": "Amit",
-      "date_created": "2024-03-08T13:21:01.810070Z"
-  },
-  {
-      "id": 3,
-      "title": "MySql",
-      "content": "most comman data container",
-      "author": "abhi",
-      "date_created": "2024-03-18T05:54:13.653406Z"
-  }
-]
- 
- 
+import axios from 'axios';
+
 class App extends Component {
- 
+  state = {
+    blogs: [] // Initialize an empty array to store fetched blog posts
+  };
+
   componentDidMount() {
-    this.refreshList()
+    this.refreshList();
   }
- 
+
   refreshList = () => {
     axios
-    .get('https://django-server-production-6614.up.railway.app/api/posts/')
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err))
-  }
- 
- 
+      .get('https://django-server-production-6614.up.railway.app/api/posts/')
+      .then((res) => {
+        // Update state with fetched blog posts
+        this.setState({ blogs: res.data });
+      })
+      .catch((err) => console.log(err));
+  };
+
   createUser = () => {
-    console.log('Crate user')
-  }
- 
+    console.log('Create blog');
+  };
+
   render() {
+    // Map over the fetched blog posts to display them
+    const blogList = this.state.blogs.map((blog) => (
+      <div key={blog.id}>
+        <h2>{blog.title}</h2>
+        <p>{blog.content}</p>
+        <p>Author: {blog.author}</p>
+        <p>Date Created: {blog.date_created}</p>
+      </div>
+    ));
+
     return (
       <div className="App">
         <main>
-          <div className='container'>
+          <div className="container">
             <h1>User App</h1>
-            <div className='row'>
-              <div className='col-md-6'>
-                <div className='mb-4'>
-                  <button
-                  className='btn btn-primary'
-                  onClick={this.createUser}
-                  >
+            <div className="row">
+              <div className="col-md-6">
+                <div className="mb-4">
+                  <button className="btn btn-primary" onClick={this.createBlog}>
                     Create Blog
                   </button>
                 </div>
               </div>
             </div>
+            {/* Display the fetched blog posts */}
+            {blogList}
           </div>
         </main>
       </div>
     );
   }
 }
- 
+
 export default App;
