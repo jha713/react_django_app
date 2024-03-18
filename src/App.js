cde76a5c9a4488ba7fbfc8a974dd21b5def8bacc
Abhishek KumarJha <jha.abhishek001@gmail.com>
2024-03-18 13:33:25 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
-      </header>
-    </div>
-  );
+import axios from 'axios'
+ 
+const Blog = [
+  {
+      "id": 1,
+      "title": "Python",
+      "content": "Python is the most used language in top company",
+      "author": "Abhishek",
+      "date_created": "2024-03-08T13:20:29.298153Z"
+  },
+  {
+      "id": 2,
+      "title": "Java",
+      "content": "Java is the most used language in top company",
+      "author": "Amit",
+      "date_created": "2024-03-08T13:21:01.810070Z"
+  },
+  {
+      "id": 3,
+      "title": "MySql",
+      "content": "most comman data container",
+      "author": "abhi",
+      "date_created": "2024-03-18T05:54:13.653406Z"
+  }
+]
+ 
+ 
+class App extends Component {
+ 
+  componentDidMount() {
+    this.refreshList()
+  }
+ 
+  refreshList = () => {
+    axios
+    .get('https://django-server-production-6614.up.railway.app/api/posts/')
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+  }
+ 
+ 
+  createUser = () => {
+    console.log('Crate user')
+  }
+ 
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <div className='container'>
+            <h1>User App</h1>
+            <div className='row'>
+              <div className='col-md-6'>
+                <div className='mb-4'>
+                  <button
+                  className='btn btn-primary'
+                  onClick={this.createUser}
+                  >
+                    Create Blog
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
